8b6727803f9997b91bc014b6f93e406c2ccda5d5
Adrian <adrian.pinzari06+github@gmail.com>
2024-11-22 22:56:29 +0200

    changed to pyperclip + allow custom port and host

--- desktop/server.py
+++ desktop/server.py
@@ -1,6 +1,8 @@
+import re
+
 from flask import Flask, request
-from subprocess import run as run_powershell
-from sys import platform, argv
+from pyperclip import copy
+from sys import argv
 app = Flask(__name__)
 
 
@@ -8,13 +10,31 @@ app = Flask(__name__)
 def main_route():
     if request.method == 'GET':
         return 'Server is OK.'
+
     if request.method == 'POST':
         new_clipboard = request.form.get("text")
-        # For computers running Windows:
-        if platform == "win32":
-            run_powershell(["powershell", "-Command", "Set-Clipboard \"", new_clipboard, "\""])
-        return str(new_clipboard)
+        if new_clipboard is not None:
+            copy(new_clipboard)
+        return 'OK'
+
     return "Invalid method."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1413)
+    default_port = 1413
+    default_host = "0.0.0.0"
+    argv_string = ' '.join(argv).lower()
+    # Search for host and port provided by user, without validating eligibility
+    host_finder_regex = r"-host\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+    port_finder_regex = r"-port\s*(\d{1,5})"
+
+    if "-host" in argv_string:
+        matches = re.search(host_finder_regex, argv_string)
+        if matches is not None:
+            default_host = matches.groups()[0]
+
+    if "-port" in argv_string:
+        matches = re.search(port_finder_regex, argv_string)
+        if matches is not None:
+            default_port = matches.groups()[0]
+
+    app.run(host=default_host, port=default_port)
